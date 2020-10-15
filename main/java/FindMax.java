08b0bd9127c8d58586ee1ea2ef7044096ed1173a
Ard2238 <abhishekrdas28@gmail.com>
2020-10-15 18:40:57 +0530

    Refactored Code to find maximum from a generic method

--- main/java/FindMax.java
+++ main/java/FindMax.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class FindMax {
     static Scanner sc = new Scanner(System.in);
 
-    public int findMaxInt(Integer num1, Integer num2, Integer num3){
-        if(num1.compareTo(num2) > 0  && num1.compareTo(num3) > 0)
-            return num1;
-        else if(num2.compareTo(num3) > 0)
-            return num2;
-        else return num3;
+    public <E extends Comparable> E findMax(E e1, E e2, E e3){
+        if(e1.compareTo(e2) > 0  && e1.compareTo(e3) > 0)
+            return e1;
+        else if(e2.compareTo(e3) > 0)
+            return e2;
+        else return e3;
     }
 
-    public float findMaxFloat(Float num4, Float num5, Float num6){
-        if(num4.compareTo(num5) > 0  && num4.compareTo(num6) > 0)
-            return num4;
-        else if(num5.compareTo(num6) > 0)
-            return num5;
-        else return num6;
-    }
+//    public float findMaxFloat(Float num4, Float num5, Float num6){
+//        if(num4.compareTo(num5) > 0  && num4.compareTo(num6) > 0)
+//            return num4;
+//        else if(num5.compareTo(num6) > 0)
+//            return num5;
+//        else return num6;
+//    }
 
-    public String findMaxString(String str1, String str2, String str3){
-        if(str1.compareTo(str2) > 0  && str1.compareTo(str2) > 0)
-            return str1;
-        else if(str2.compareTo(str3) > 0)
-            return str2;
-        else return str3;
-    }
+//    public String findMaxString(String str1, String str2, String str3){
+//        if(str1.compareTo(str2) > 0  && str1.compareTo(str2) > 0)
+//            return str1;
+//        else if(str2.compareTo(str3) > 0)
+//            return str2;
+//        else return str3;
+//    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Find Maximum Program");
@@ -35,19 +35,19 @@ public class FindMax {
         Integer num1 = sc.nextInt();
         Integer num2 = sc.nextInt();
         Integer num3 = sc.nextInt();
-        System.out.println("Max Integer : " + findMax.findMaxInt(num1, num2, num3));
+        System.out.println("Max Integer : " + findMax.findMax(num1, num2, num3));
 
         System.out.println("Enter the three numbers : ");
         Float num4 = sc.nextFloat();
         Float num5 = sc.nextFloat();
         Float num6 = sc.nextFloat();
-        System.out.println("Max Float value : " + findMax.findMaxFloat(num4, num5, num6));
+        System.out.println("Max Float value : " + findMax.findMax(num4, num5, num6));
 
         System.out.println("Enter the three strings : ");
         String str1 = sc.next();
         String str2 = sc.next();
         String str3 = sc.next();
-        System.out.println("Max String value : " + findMax.findMaxString(str1, str2, str3));
+        System.out.println("Max String value : " + findMax.findMax(str1, str2, str3));
 
     }
 }
--- test/java/TestFindMax.java
+++ test/java/TestFindMax.java
@@ -10,22 +10,22 @@ public class TestFindMax {
     }
     @Test
     public void testFindMaxInt(){
-        Assert.assertEquals(9, findMax.findMaxInt(9,3,7));
-        Assert.assertEquals(9, findMax.findMaxInt(3,9,7));
-        Assert.assertEquals(9,findMax.findMaxInt(3,7,9));
+        Assert.assertEquals((Integer)9, findMax.findMax(9,3,7));
+        Assert.assertEquals((Integer)9, findMax.findMax(3,9,7));
+        Assert.assertEquals((Integer)9,findMax.findMax(3,7,9));
     }
 
     @Test
     public void testFindMaxFloat(){
-        Assert.assertEquals(11.85, findMax.findMaxFloat(11.85f,5.96f,7.31f), 2);
-        Assert.assertEquals(11.85, findMax.findMaxFloat(5.96f,11.85f,7.31f), 2);
-        Assert.assertEquals(11.85,findMax.findMaxFloat(5.96f,7.31f,11.85f), 2);
+        Assert.assertEquals(11.85, findMax.findMax(11.85f,5.96f,7.31f), 2);
+        Assert.assertEquals(11.85, findMax.findMax(5.96f,11.85f,7.31f), 2);
+        Assert.assertEquals(11.85,findMax.findMax(5.96f,7.31f,11.85f), 2);
     }
 
     @Test
     public void testFindMaxString(){
-        Assert.assertEquals("Peach", findMax.findMaxString("Peach", "Apple", "Banana"));
-        Assert.assertEquals("Peach", findMax.findMaxString("Apple", "Peach", "Banana"));
-        Assert.assertEquals("Peach", findMax.findMaxString("Apple", "Banana", "Peach"));
+        Assert.assertEquals("Peach", findMax.findMax("Peach", "Apple", "Banana"));
+        Assert.assertEquals("Peach", findMax.findMax("Apple", "Peach", "Banana"));
+        Assert.assertEquals("Peach", findMax.findMax("Apple", "Banana", "Peach"));
     }
 }
