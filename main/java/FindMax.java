1ab5dc9bcb9c77ac3c2be6fd5f6d8a60faec9198
Ard2238 <abhishekrdas28@gmail.com>
2020-10-15 18:24:03 +0530

    Added Code to find maximum among 3 float values

--- main/java/FindMax.java
+++ main/java/FindMax.java
@@ -10,6 +10,15 @@ public class FindMax {
             return num2;
         else return num3;
     }
+
+    public float findMaxFloat(Float num4, Float num5, Float num6){
+        if(num4.compareTo(num5) > 0  && num4.compareTo(num6) > 0)
+            return num4;
+        else if(num5.compareTo(num6) > 0)
+            return num5;
+        else return num6;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Find Maximum Program");
         FindMax findMax = new FindMax();
@@ -19,6 +28,12 @@ public class FindMax {
         Integer num2 = sc.nextInt();
         Integer num3 = sc.nextInt();
         System.out.println("Max Integer : " + findMax.findMaxInt(num1, num2, num3));
+
+        System.out.println("Enter the three numbers : ");
+        Float num4 = sc.nextFloat();
+        Float num5 = sc.nextFloat();
+        Float num6 = sc.nextFloat();
+        System.out.println("Max Float value : " + findMax.findMaxFloat(num4, num5, num6));
 
     }
 }
